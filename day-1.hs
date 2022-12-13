ebb91cac78d45d2965a38187d52224ba6066c417
Charles Saineghi <charles@delltoo.localdomain>
2022-12-13 10:52:21 -0600

    make day 1 better :penguin:

--- day-1.hs
+++ day-1.hs
@@ -6,15 +6,16 @@ import Data.List
 
 filename = "day-1.txt"
 
-f :: [[Int]] -> [[String]] -> [[Int]] -- hell
-f _ [] = []
-f c (x:xs) = (map (read :: String -> Int) x) : (f c xs)
-
+hell :: [[Int]] -> [[String]] -> [[Int]] -- hell
+hell _ [] = []
+hell c (x:xs) = (map (read :: String -> Int) x) : (hell c xs)
 
 main :: IO ()
 main = do
     let list = []
     handle <- openFile filename ReadMode
     contents <- hGetContents handle
-    print $ sum $ take 3 $ (reverse . sort) (map sum (f [[]] (splitWhen (=="") (lines contents))))
+    let l = (reverse . sort) $ (map sum (hell [[]] (splitWhen (=="") (lines contents))))
+    print $ head l -- Part 1
+    print $ (sum . take 3) l -- Part 2
     hClose handle
