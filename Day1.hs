815ae8cefee184c8a5cff7cb449078867395d3c2
Charles Saineghi <charles@delltoo.localdomain>
2022-12-15 18:16:23 -0600

    day 2 part one :beans:

--- Day1.hs
+++ Day1.hs
@@ -3,18 +3,18 @@ import Control.Monad
 import Data.List.Split
 import Data.List
 
+filename :: String
 filename = "Day1.txt"
 
 hell :: [[Int]] -> [[String]] -> [[Int]] -- hell
 hell _ [] = []
-hell c (x:xs) = (map (read :: String -> Int) x) : (hell c xs)
+hell c (x:xs) = map (read :: String -> Int) x : hell c xs
 
 main :: IO ()
 main = do
-    let list = []
     handle <- openFile filename ReadMode
     contents <- hGetContents handle
-    let l = (reverse . sort) $ (map sum (hell [[]] (splitWhen (=="") (lines contents))))
+    let l = (reverse . sort) $ map sum $ hell [[]] $ splitWhen (=="") (lines contents)
     print $ head l -- Part 1
     print $ (sum . take 3) l -- Part 2
     hClose handle
--- Day2.hs
+++ Day2.hs
@@ -1,12 +1,68 @@
+{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE InstanceSigs #-}
 import System.IO
 import Control.Monad
 
-filename = "day-2.txt"
+filename :: String
+filename = "Day2.txt"
+
+data Verdict = Win | Loss | Tie | Mmm
+data Shape = Rock | Paper | Scissors
+
+instance Show Verdict where
+    show :: Verdict -> String
+    show Win = "Win"
+    show Loss = "Loss"
+    show Tie = "Tie"
+    show Mmm = "Mmm"
+
+instance Show Shape where
+    show :: Shape -> String
+    show Rock = "Rock"
+    show Paper = "Paper"
+    show Scissors = "Scissors"
+
+(===) :: Shape -> Shape -> Verdict
+Rock === Rock = Tie         -- Rock/Rock
+Paper === Rock = Loss       -- Paper/Rock
+Scissors === Rock = Win     -- Scissors/Rock
+Rock === Paper = Win        -- Rock/Paper
+Paper === Paper = Tie       -- Paper/Paper
+Scissors === Paper = Loss   -- Scissors/Paper
+Rock === Scissors = Loss    -- Rock/Scissors
+Paper === Scissors = Win    -- Paper/Scissors
+Scissors === Scissors = Tie -- Scissors/Scissors
+
+-- A and X are Rock
+-- B and Y are Paper
+-- C and Z are Scissors
+
+getShapeFromLetter :: String -> Shape -- extract shape from string
+getShapeFromLetter x | x == "A" || x == "X" = Rock
+                     | x == "B" || x == "Y" = Paper
+                     | x == "C" || x == "Z" = Scissors
+                     | otherwise = undefined
+
+getVerdict :: [Shape] -> Verdict
+getVerdict x = head x === last x
+
+pointsOfVerdict :: Verdict -> Int
+pointsOfVerdict Win = 6
+pointsOfVerdict Loss = 0
+pointsOfVerdict Tie = 3
+
+pointsOfShape :: Shape -> Int
+pointsOfShape Rock = 1
+pointsOfShape Paper = 2
+pointsOfShape Scissors = 3
+
+getPoints :: [Shape] -> Int
+getPoints x = pointsOfShape (last x) + pointsOfVerdict (getVerdict x)
 
 main :: IO ()
 main = do
     let list = []
     handle <- openFile filename ReadMode
     contents <- hGetContents handle
-    print $ lines contents
+    print $ sum $ map (getPoints . (map getShapeFromLetter . words)) $ lines contents
     hClose handle
